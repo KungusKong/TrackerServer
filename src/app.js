d39b0b6f18c0c2826776c619432a73ad4053b04a
KungusKong <88599594+KungusKong@users.noreply.github.com>
2021-10-02 16:35:29 -0500

    Base Commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"]
+  }
+});
